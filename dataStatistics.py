a3994071f02732134efb59684e37509623671659
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2017-11-02 13:17:30 +0100

    Add files via upload

--- dataStatistics.py
+++ dataStatistics.py
@@ -26,19 +26,16 @@ def dataStatistics(data, statistic):
         result = len(data[:,0])
     #Calculate average growth rate when T < 20
     elif statistic == "mean cold growth rate":
-        try:
-            data = data[np.where(data[:,0] < 20)] #find the rows in the matrix that satisfies the given interval
-            result = np.mean(data[:,1]) #take the average value of the remaining column
-        except #noget med NaN: #if there is no temperature <20, return error message
-            print("No temperature lower than 20 degrees.")           
+        data = data[np.where(data[:,0] < 20)] #find the rows in the matrix that satisfies the given interval
+        result = np.mean(data[:,1]) #take the average value of the remaining column         
     #Calculate average growth rate when T > 50
     elif statistic == "mean hot growth rate":
-        try:
-            data = data[np.where(data[:,0] > 50)] #find the rows in the matrix that satisfies the given interval
-            result = np.mean(data[:,1]) #take the average value of the remaining column
-        except #noget med NaN: #if there is no temperature <20, return error message
-            print("No temperature higher than 50 degrees.")
+        data = data[np.where(data[:,0] > 50)] #find the rows in the matrix that satisfies the given interval
+        result = np.mean(data[:,1]) #take the average value of the remaining column
     else:
-        print("Input not valid. Try again!")
+        pass
+        #Error if "statistic" is not one of the choices above
+
+#Noticed error possibilities: If array in "mean cold growth rate" or "mean hot growth rate" is empty, output is "nan"
     
     return result
